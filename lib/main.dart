1a888831339c52ee5d2aad12a913824cb73034db
Jace Babin <jbabin91@gmail.com>
2018-05-21 16:24:31 -0700

    finished circles and cleaned up main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_forecast/forecast/background/background_with_rings.dart';
 
 void main() => runApp(new MyApp());
 
@@ -25,44 +26,9 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return new Scaffold(
         body: new Stack(
-      fit: StackFit.expand,
       children: <Widget>[
-        new Image.asset(
-          'assets/weather-bk_enlarged.png',
-          fit: BoxFit.cover,
-        ),
-        new ClipOval(
-          clipper:
-              new CircleClipper(radius: 140.0, offset: const Offset(40.0, 0.0)),
-          child: new Image.asset(
-            'assets/weather-bk.png',
-            fit: BoxFit.cover,
-          ),
-        )
+        new BackgroundWithRings(),
       ],
     ));
-  }
-}
-
-class CircleClipper extends CustomClipper<Rect> {
-  final double radius;
-  final Offset offset;
-
-  CircleClipper({
-    this.radius,
-    this.offset = const Offset(0.0, 0.0),
-  });
-
-  @override
-  Rect getClip(Size size) {
-    return new Rect.fromCircle(
-      center: new Offset(0.0, size.height / 2) + offset,
-      radius: radius,
-    );
-  }
-
-  @override
-  bool shouldReclip(CustomClipper<Rect> oldClipper) {
-    return true;
   }
 }
